e878053d53b4a39fe4959052ce6850dbcad61f60
Fabio Gomes <fabio.gomes@intranet.gazin.com.br>
2016-09-26 10:24:57 -0300

    carsale commit

---
+++ importados.php
@@ -0,0 +1,78 @@
+
+<div class="well">
+	<div class="container">
+		<h1>Importados</h1>
+		<p>Confira os 8 carros importados mais vendidos no Brasil.</p>
+	</div>
+</div> <!-- fim do well -->
+
+<div class="container">
+	<h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Destaques mês de Agosto:</h2>
+	<div class="row">
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/landrover.jpg" alt="landrover" class="img-responsive">
+			<h3>Land Rover</h3>
+			<a href="landrover" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/landroversport.jpg" alt="landroversport" class="img-responsive">
+			<h3>Land Rover Sport</h3>
+			<a href="landroversport" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/newcerato.jpg" alt="newcerato" class="img-responsive">
+			<h3>New Cerato</h3>
+			<a href="newcerato" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/sportage.png" alt="sportage" class="img-responsive">
+			<h3>Sportage</h3>
+			<a href="sportage" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+	</div> <!-- fim do row -->
+        
+        <h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Destaques mês de Setembro:</h2>
+        <div class="row">
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/sw4.jpg" alt="sw4" class="img-responsive">
+			<h3>SW4</h3>
+			<a href="sw4" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/t5.jpg" alt="t5" class="img-responsive">
+			<h3>JAC T5</h3>
+			<a href="t5" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/vitara.jpg" alt="vitara" class="img-responsive">
+			<h3>Vitara</h3>
+			<a href="vitara" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/importados/volvo.jpg" alt="volvo" class="img-responsive">
+			<h3>Volvo XC60</h3>
+			<a href="volvo" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+	</div>
+</div> <!-- fim do container -->
+
+
+
+
+
---
+++ nacionais.php
@@ -0,0 +1,78 @@
+
+<div class="well">
+	<div class="container">
+		<h1>Nacionais</h1>
+		<p>Os 8 carros brasileiros mais econômicos de 2016</p>
+	</div>
+</div> <!-- fim do well -->
+
+<div class="container">
+	<h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Destaques mês de Agosto:</h2>
+	<div class="row">
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/peugeot-208.jpg" alt="peugeot" class="img-responsive">
+			<h3>Peugeot-208</h3>
+			<a href="peugeot" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/nissanversa.jpg" alt="nissanversa" class="img-responsive">
+			<h3>Nissan Versa</h3>
+			<a href="nissanversa" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/fordka.jpg" alt="fordka" class="img-responsive">
+			<h3>Ford Ka</h3>
+			<a href="fordka" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/voyage.jpg" alt="voyage" class="img-responsive">
+			<h3>Voyage</h3>
+			<a href="voyage" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+	</div> <!-- fim do row -->
+        
+        <h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Destaques mês de Setembro:</h2>
+        <div class="row">
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/gol.jpg" alt="gol" class="img-responsive">
+			<h3>Gol</h3>
+			<a href="gol" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/fiatuno.jpg" alt="fiatuno" class="img-responsive">
+			<h3>Fiat Uno</h3>
+			<a href="fiatuno" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/fiatmobi.jpg" alt="fiatmobi" class="img-responsive">
+			<h3>Fiat Mobi</h3>
+			<a href="fiatmobi" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+                    <img src="imgs/nacionais/fiatpalio.jpg" alt="fiatpalio" class="img-responsive">
+			<h3>Fiat Palio</h3>
+			<a href="fiatpalio" class="btn btn-danger">
+				<i class="fa fa-search"></i> Detalhes
+			</a>
+		</div>
+	</div>
+</div> <!-- fim do container -->
+
+
+
+
+
